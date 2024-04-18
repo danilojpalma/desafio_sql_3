6c76c10659002ad65ecbbecf994adc3892801097
Danilo J. Palma <dan.palmav@duocuc.cl>
2024-04-17 20:21:26 -0400

    :tada: consultas para base de datos

---
+++ peliculas.sql
@@ -0,0 +1,30 @@
+-- Obtener el ID de la película “Titanic”.
+select id from peliculas where "Pelicula" = 'Titanic';
+
+-- Listar a todos los actores que aparecen en la película "Titanic
+select reparto.actor
+from peliculas
+join reparto
+on peliculas.id = reparto.id_pelicula
+where peliculas."Pelicula"  = 'Titanic';
+
+-- Consultar en cuántas películas del top 100 participa Harrison Ford.
+select count(peliculas."Pelicula")
+from peliculas
+join reparto
+on peliculas.id = reparto.id_pelicula
+where reparto.actor  = 'Harrison Ford';
+
+-- Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de manera ascendente.
+select "Pelicula" 
+from peliculas 
+where "Año estreno" between 1990 and 1999
+order by "Pelicula" asc;
+
+-- Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser nombrado para la consulta como “longitud_titulo”.
+select "Pelicula", length("Pelicula") as "longitud_titulo"
+from peliculas;
+
+-- Consultar cual es la longitud más grande entre todos los títulos de las películas.
+select max(length("Pelicula")) as "longitud_titulo"
+from peliculas;
